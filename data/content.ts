aa3061aba86a0797bacb728d48bf02af0ad8a8d0
ai slave <6696080+ai-slave@users.noreply.github.com>
2023-03-31 16:52:41 +0700

    data separated

---
+++ data/content.ts
@@ -0,0 +1,155 @@
+export type Section = {
+    id: string
+    layout?: 'main' | 'section' | 'footer' | 'about'
+    title: string
+    description: string
+    link?: string
+    hidden?: boolean
+    backgroundClass: string
+    logo?: {
+        w: number
+        h: number
+    }
+}
+
+export const sections: Section[] = [
+    {
+        id: 'Delv',
+        layout: 'main',
+        title: 'The Factory for DeFi',
+        description: 'Delv is developing the complete suite of decentralized finance. From core infrastructure to structured products, our protocols work together to help create and usher in the new financial system.',
+        backgroundClass: 'bg-white',
+    },
+    {
+        id: 'Element',
+        title: 'Fixed rate protocol',
+        description: 'It all began with Element, an open-source decentralized protocol for fixed and variable yield markets.',
+        link: 'https://element.fi',
+        backgroundClass: 'bg-white',
+        logo: {
+            w: 325,
+            h: 64,
+        }
+    },
+    {
+        id: 'Council',
+        title: 'Council protocol',
+        description: 'Council is a decentralized governance system and suite of tools that allows a community to deploy and manage a DAO. It represents a new era of governance innovation, allowing anyone to build adaptable governance systems using the security of on-chain governance while allowing for unprecedented modularity and flexibility.',
+        link: 'https://council.element.fi/',
+        backgroundClass: 'bg-black',
+        logo: {
+            w: 400,
+            h: 52,
+        }
+    },
+    {
+        id: 'Elfiverse',
+        title: 'NFTs meet DeFi',
+        description: 'The lore of the Element DAO was born with Elfiverse – an endeavor to intersect the DeFi and NFT worlds while catalyzing network effects and community building. Elfiverse pushes the boundaries of on-chain governance allowing communities to look beyond the 1-token-1-vote system, driving inclusive governance participation for all.',
+        link: 'https://elfiverse.element.fi/',
+        backgroundClass: 'bg-black',
+        logo: {
+            w: 384,
+            h: 72,
+        }
+    },
+    {
+        id: 'Hyperdrive',
+        title: 'A new way to trade rates',
+        description: 'Hyperdrive is the next research leap from Delv on variable and fixed rate primitives. It is an advanced AMM featuring no preset expiration dates, no fragmented liquidity, and no LP rollovers — aka everlasting liquidity.',
+        backgroundClass: 'bg-black',
+        link: 'https://hyperdrive.element.fi/',
+        logo: {
+            w: 400,
+            h: 174,
+        }
+    },
+    {
+        id: 'Echo',
+        title: 'Browser-Layer P2P',
+        description: 'Echo is a new P2P protocol that focuses on the browser-layer. It enables visitors of web3 applications to peer/host infrastructure needed to power the application. Echo is a natural progression for Council, allowing DAOs to run frontends and data services. It enables a world of truly decentralized protocols and DAOs.',
+        backgroundClass: 'bg-black',
+        logo: {
+            w: 286,
+            h: 101,
+        }
+    },
+    {
+        id: 'Agent_0',
+        title: 'Data Simulation',
+        description: 'Agent_0 is a data simulation framework to aid progress on the new Hyperdrive AMM design before implementing it in Solidity. The models include smart agents that allow us to graph/stress test against all scenarios. It will also leverage reinforcement machine learning to further our research endeavors.',
+        backgroundClass: 'bg-black',
+        logo: {
+            w: 400,
+            h: 97,
+        }
+    },
+    {
+        id: 'About',
+        hidden: true,
+        layout: 'about',
+        title: 'Build with us',
+        description: 'Let’s reimagine the future of finance together.',
+        backgroundClass: 'bg-black',
+    }
+]
+
+export const Links: {
+    [key: string]: {
+        name: string;
+        url: string;
+    }[];
+} = {
+    "Build": [
+        {
+            "name": "Documentation",
+            "url": "https://docs.element.fi/"
+        },
+        {
+            "name": "White paper",
+            "url": "https://paper.element.fi//"
+        },
+        {
+            "name": "Tutorial",
+            "url": "https://medium.com/element-finance/how-to-access-high-fixed-apr-on-usd-eth-and-btc-in-4-minutes-641f057e283b"
+        },
+        {
+            "name": "Fixed interest",
+            "url": "https://medium.com/element-finance/fixed-rate-interest-markets-a-casual-users-journey-through-fixed-rate-interest-using-element-50f420df1859"
+        },
+        {
+            "name": "Bug bounty",
+            "url": "https://immunefi.com/bounty/elementfinance/"
+        },
+        {
+            "name": "Brand assets",
+            "url": "https://github.com/element-fi/brand-assets"
+        },
+        {
+            "name": "Jobs",
+            "url": "tbc"
+        }
+    ],
+    "Community": [
+        {
+            "name": "Twitter",
+            "url": "https://twitter.com/element_fi"
+        },
+        {
+            "name": "Discord",
+            "url": "https://discord.gg/EEfKmfQdtx"
+        },
+        {
+            "name": "Blog",
+            "url": "https://blog.element.fi/"
+        },
+        {
+            "name": "Github",
+            "url": "https://github.com/element-fi"
+        },
+        {
+            "name": "YouTube",
+            "url": "https://www.youtube.com/channel/UCwxX4xrw-AZF_7dB7gVMaSw"
+        }
+    ]
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -7,275 +7,114 @@ import { motion, AnimatePresence } from "framer-motion"
 import { useState, useEffect, useRef, MutableRefObject } from 'react'
 import { ProjectBackground } from '@/components/ProjectBackground'
 
-type Section = {
-  id: string
-  layout?: 'main' | 'section' | 'footer' | 'about'
-  title: string
-  description: string
-  link?: string
-  hidden?: boolean
-  backgroundClass: string
-  logo?: {
-    w: number
-    h: number
-  }
-}
+import { sections, Links } from '@/data/content';
+
 
 const useScrollDirection = (
-    activeSection: number, setActiveSection: React.Dispatch<React.SetStateAction<number>>, 
-    caretPosition: number, setCaretPosition: React.Dispatch<React.SetStateAction<number>>,
-    isScrolling: boolean, setIsScrolling: React.Dispatch<React.SetStateAction<boolean>>) => {
-  const [touchStartY, setTouchStartY] = useState<number | null>(null);
+  activeSection: number, setActiveSection: React.Dispatch<React.SetStateAction<number>>, 
+  caretPosition: number, setCaretPosition: React.Dispatch<React.SetStateAction<number>>,
+  isScrolling: boolean, setIsScrolling: React.Dispatch<React.SetStateAction<boolean>>) => {
+const [touchStartY, setTouchStartY] = useState<number | null>(null);
 
-  const isInsideSections = (element: Element | null): boolean => {
-    while (element) {
-      if (element.classList && element.classList.contains('sections') || element.classList.contains('mobile_nav')) {
-        return true;
-      }
-      element = element.parentElement;
+const isInsideSections = (element: Element | null): boolean => {
+  while (element) {
+    if (element.classList && element.classList.contains('sections') || element.classList.contains('mobile_nav')) {
+      return true;
     }
-    return false;
+    element = element.parentElement;
+  }
+  return false;
+};
+useEffect(() => {
+  let scrollTimeout: any = null;
+  const handleScroll = (event: WheelEvent) => {
+    if (isScrolling 
+      // || isInsideSections(event.target as HTMLElement)
+      ) {
+      return;
+    }
+    caretPosition += event.deltaY / 3;
+    if (caretPosition < 0) {
+      caretPosition = 0;
+    } else if (caretPosition > 34*(sections.length - 2)) {
+      caretPosition = 34*(sections.length - 2);
+    }
+    setCaretPosition(caretPosition);
+    if (scrollTimeout) {
+      clearTimeout(scrollTimeout);
+    }
+    scrollTimeout = setTimeout(() => {
+      let section: number = Math.round(caretPosition/34);
+
+      setCaretPosition(section*34);
+      setActiveSection(section);
+    }, 200);
   };
-  useEffect(() => {
-    let scrollTimeout: any = null;
-    const handleScroll = (event: WheelEvent) => {
-      if (isScrolling 
-        // || isInsideSections(event.target as HTMLElement)
-        ) {
-        return;
+
+  const handleTouchStart = (event: TouchEvent) => {
+    setTouchStartY(event.touches[0].clientY);
+  };
+
+  const handleTouchMove = (event: TouchEvent) => {
+    if (!touchStartY || isInsideSections(event.target as HTMLElement)) return;
+    if (!touchStartY) return;
+
+    const touchEndY = event.touches[0].clientY;
+    const deltaY = touchStartY - touchEndY;
+
+    if (Math.abs(deltaY) > 50) { // Adjust this value for sensitivity
+      if (deltaY > 0) {
+        handleScrollDown();
+      } else {
+        handleScrollUp();
       }
-      caretPosition += event.deltaY / 3;
-      // caretPosition min 0 max 34*sections.length
-      if (caretPosition < 0) {
-        caretPosition = 0;
-      } else if (caretPosition > 34*(sections.length - 2)) {
-        caretPosition = 34*(sections.length - 2);
-      }
-      setCaretPosition(caretPosition);
-      if (scrollTimeout) {
-        clearTimeout(scrollTimeout);
-      }
-      scrollTimeout = setTimeout(() => {
-        let section: number = Math.round(caretPosition/34);
-        // let leftOver = caretPosition%34;
-        // if (leftOver > 23) {
-        //   section += 1;
-        // }
-
-        setCaretPosition(section*34);
-        setActiveSection(section);
-      }, 200);
-    };
-
-    const handleTouchStart = (event: TouchEvent) => {
-      setTouchStartY(event.touches[0].clientY);
-    };
-
-    const handleTouchMove = (event: TouchEvent) => {
-      if (!touchStartY || isInsideSections(event.target as HTMLElement)) return;
-      if (!touchStartY) return;
-
-      const touchEndY = event.touches[0].clientY;
-      const deltaY = touchStartY - touchEndY;
-
-      if (Math.abs(deltaY) > 50) { // Adjust this value for sensitivity
-        if (deltaY > 0) {
-          handleScrollDown();
-        } else {
-          handleScrollUp();
-        }
-        setTouchStartY(null);
-        setIsScrolling(true);
-        setTimeout(() => {
-          setIsScrolling(false);
-        }, 2000);
-      }
-    };
-
-    const handleTouchEnd = (event: TouchEvent) => {
       setTouchStartY(null);
-    };
-
-    window.addEventListener('wheel', handleScroll);
-    window.addEventListener('touchstart', handleTouchStart);
-    window.addEventListener('touchmove', handleTouchMove);
-    window.addEventListener('touchend', handleTouchEnd);
-
-    return () => {
-      window.removeEventListener('wheel', handleScroll);
-      window.removeEventListener('touchstart', handleTouchStart);
-      window.removeEventListener('touchmove', handleTouchMove);
-      window.removeEventListener('touchend', handleTouchEnd);
-    };
-  }, [activeSection, isScrolling, touchStartY]);
-
-
-  // on change of activeSection, change caret position
-  useEffect(() => {
-    setCaretPosition(activeSection*34);
-  }, [activeSection]);
-  
-  const handleScrollUp = () => {
-    if (activeSection > 0) {
-      setActiveSection(activeSection - 1);
-    } else {
-      setActiveSection(sections.length - 2);
+      setIsScrolling(true);
+      setTimeout(() => {
+        setIsScrolling(false);
+      }, 2000);
     }
   };
 
-  const handleScrollDown = () => {
-    if (activeSection < sections.length - 2) {
-      setActiveSection(activeSection + 1);
-    } else {
-      setActiveSection(0);
-    }
+  const handleTouchEnd = (event: TouchEvent) => {
+    setTouchStartY(null);
   };
+
+  window.addEventListener('wheel', handleScroll);
+  window.addEventListener('touchstart', handleTouchStart);
+  window.addEventListener('touchmove', handleTouchMove);
+  window.addEventListener('touchend', handleTouchEnd);
+
+  return () => {
+    window.removeEventListener('wheel', handleScroll);
+    window.removeEventListener('touchstart', handleTouchStart);
+    window.removeEventListener('touchmove', handleTouchMove);
+    window.removeEventListener('touchend', handleTouchEnd);
+  };
+}, [activeSection, isScrolling, touchStartY]);
+
+
+// on change of activeSection, change caret position
+useEffect(() => {
+  setCaretPosition(activeSection*34);
+}, [activeSection]);
+
+const handleScrollUp = () => {
+  if (activeSection > 0) {
+    setActiveSection(activeSection - 1);
+  } else {
+    setActiveSection(sections.length - 2);
+  }
 };
 
-
-
-
-const sections: Section[] = [
-  {
-    id: 'Delv',
-    layout: 'main',
-    title: 'The Factory for DeFi',
-    description: 'Delv is developing the complete suite of decentralized finance. From core infrastructure to structured products, our protocols work together to help create and usher in the new financial system.',
-    backgroundClass: 'bg-white',
-  },
-  {
-    id: 'Element',
-    title: 'Fixed rate protocol',
-    description: 'It all began with Element, an open-source decentralized protocol for fixed and variable yield markets.',
-    link: 'https://element.fi',
-    backgroundClass: 'bg-white',
-    logo: {
-      w: 325,
-      h: 64,
-    }
-  },
-  {
-    id: 'Council',
-    title: 'Council protocol',
-    description: 'Council is a decentralized governance system and suite of tools that allows a community to deploy and manage a DAO. It represents a new era of governance innovation, allowing anyone to build adaptable governance systems using the security of on-chain governance while allowing for unprecedented modularity and flexibility.',
-    link: 'https://council.element.fi/',
-    backgroundClass: 'bg-black',
-    logo: {
-      w: 400,
-      h: 52,
-    }
-  },
-  {
-    id: 'Elfiverse',
-    title: 'NFTs meet DeFi',
-    description: 'The lore of the Element DAO was born with Elfiverse – an endeavor to intersect the DeFi and NFT worlds while catalyzing network effects and community building. Elfiverse pushes the boundaries of on-chain governance allowing communities to look beyond the 1-token-1-vote system, driving inclusive governance participation for all.',
-    link: 'https://elfiverse.element.fi/',
-    backgroundClass: 'bg-black',
-    logo: {
-      w: 384,
-      h: 72,
-    }
-  },
-  {
-    id: 'Hyperdrive',
-    title: 'A new way to trade rates',
-    description: 'Hyperdrive is the next research leap from Delv on variable and fixed rate primitives. It is an advanced AMM featuring no preset expiration dates, no fragmented liquidity, and no LP rollovers — aka everlasting liquidity.',
-    backgroundClass: 'bg-black',
-    link: 'https://hyperdrive.element.fi/',
-    logo: {
-      w: 400,
-      h: 174,
-    }
-  },
-  {
-    id: 'Echo',
-    title: 'Browser-Layer P2P',
-    description: 'Echo is a new P2P protocol that focuses on the browser-layer. It enables visitors of web3 applications to peer/host infrastructure needed to power the application. Echo is a natural progression for Council, allowing DAOs to run frontends and data services. It enables a world of truly decentralized protocols and DAOs.',
-    backgroundClass: 'bg-black',
-    logo: {
-      w: 286,
-      h: 101,
-    }
-  },
-  {
-    id: 'Agent_0',
-    title: 'Data Simulation',
-    description: 'Agent_0 is a data simulation framework to aid progress on the new Hyperdrive AMM design before implementing it in Solidity. The models include smart agents that allow us to graph/stress test against all scenarios. It will also leverage reinforcement machine learning to further our research endeavors.',
-    backgroundClass: 'bg-black',
-    logo: {
-      w: 400,
-      h: 97,
-    }
-  },
-  {
-    id: 'About',
-    hidden: true,
-    layout: 'about',
-    title: 'Build with us',
-    description: 'Let’s reimagine the future of finance together.',
-    backgroundClass: 'bg-black',
+const handleScrollDown = () => {
+  if (activeSection < sections.length - 2) {
+    setActiveSection(activeSection + 1);
+  } else {
+    setActiveSection(0);
   }
-]
-
-const Links: {
-  [key: string]: {
-    name: string;
-    url: string;
-  }[];
-} = {
-  "Build": [
-    {
-      "name": "Documentation",
-      "url": "https://docs.element.fi/"
-    },
-    {
-      "name": "White paper",
-      "url": "https://paper.element.fi//"
-    },
-    {
-      "name": "Tutorial",
-      "url": "https://medium.com/element-finance/how-to-access-high-fixed-apr-on-usd-eth-and-btc-in-4-minutes-641f057e283b"
-    },
-    {
-      "name": "Fixed interest",
-      "url": "https://medium.com/element-finance/fixed-rate-interest-markets-a-casual-users-journey-through-fixed-rate-interest-using-element-50f420df1859"
-    },
-    {
-      "name": "Bug bounty",
-      "url": "https://immunefi.com/bounty/elementfinance/"
-    },
-    {
-      "name": "Brand assets",
-      "url": "https://github.com/element-fi/brand-assets"
-    },
-    {
-      "name": "Jobs",
-      "url": "tbc"
-    }
-  ],
-  "Community": [
-    {
-      "name": "Twitter",
-      "url": "https://twitter.com/element_fi"
-    },
-    {
-      "name": "Discord",
-      "url": "https://discord.gg/EEfKmfQdtx"
-    },
-    {
-      "name": "Blog",
-      "url": "https://blog.element.fi/"
-    },
-    {
-      "name": "Github",
-      "url": "https://github.com/element-fi"
-    },
-    {
-      "name": "YouTube",
-      "url": "https://www.youtube.com/channel/UCwxX4xrw-AZF_7dB7gVMaSw"
-    }
-  ]
-}
+};
+};
 
 // preload images using head and link preload
 const preloadImages = () => {
